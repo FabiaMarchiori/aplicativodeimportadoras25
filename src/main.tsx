0145d44c12574a7e751d027ce575d52cc2c2bde6
FabiaMarchiori <fasilva73@hotmail.com>
2025-07-17 17:49:57 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+// 👇 Importa a função que registra o service worker
+import { registerServiceWorker } from './serviceWorkerRegistration';
 
 createRoot(document.getElementById("root")!).render(<App />);
+
+// 👇 Chama a função para ativar o service worker
+registerServiceWorker();
